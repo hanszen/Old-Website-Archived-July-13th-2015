546ddebf45c66dfcfca9c9a556dafd071ce2d9ce
Avery Whitaker <averywhitaker@gmail.com>
2016-04-18 16:22:32 -0500

    Downloaded from archive.org with wayback_machine_downloader ruby gem

---
+++ cgi-bin/directory.pl
@@ -0,0 +1,212 @@
+
+<html>
+<header>
+  <title>Virtual Directory</title>
+</header>
+
+<body background=/Images/Background/gray-texture.gif>
+
+<center>
+  <table>
+    <tr>
+      <td valign=center>
+        <img src=/Images/crest-small.gif>
+      </td>
+      <td><b><font size=+3>Virtual Directory</font></b></td>
+      <td valign=center>
+        <img src=/Images/riceseal-small.gif>
+      </td>
+    </tr>
+  </table>
+</center>
+
+
+
+<CENTER>
+<b>Note:  To find Hanszenites on Owlnet, try
+  <a target=_top href=http://www.rice.edu/webshare_all.html#7>here</a>.
+</b>
+<br>
+<br>
+
+<TABLE ALIGN=CENTER>
+<TR><TD><b>Joyce Lynn Almaguer</b></TD>
+<TD><A HREF="mailto:joycee@rice.edu">joycee@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~ston/"><b>Eitan Yaakov Altman</b></A></TD>
+<TD><A HREF="mailto:ston@rice.edu">ston@rice.edu</A></TD></TR>
+<TR><TD><b>Catherine Michelle Arthur</b></TD>
+<TD><A HREF="mailto:cma@rice.edu">cma@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~drwho/"><b>David Matthew Bortz</b></A></TD>
+<TD><A HREF="mailto:drwho@rice.edu">drwho@rice.edu</A></TD></TR>
+<TR><TD><b>Orlando Jose Cardoso</b></TD>
+<TD><A HREF="mailto:orlando@rice.edu">orlando@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~schan/"><b>Sabrina Szying Chan</b></A></TD>
+<TD><A HREF="mailto:schan@rice.edu">schan@rice.edu</A></TD></TR>
+<TR><TD><b>Seema Chandra</b></TD>
+<TD><A HREF="mailto:seemac@rice.edu">seemac@rice.edu</A></TD></TR>
+<TR><TD><b>Stewart Tzu-Chien Chang</b></TD>
+<TD><A HREF="mailto:stewartc@rice.edu">stewartc@rice.edu</A></TD></TR>
+<TR><TD><b>Henry Haipei Chen</b></TD>
+<TD><A HREF="mailto:assman@rice.edu">assman@rice.edu</A></TD></TR>
+<TR><TD><b>Henry Haipei Chen</b></TD>
+<TD><A HREF="mailto:hchen@rice.edu">hchen@rice.edu</A></TD></TR>
+<TR><TD><b>Jonathan Sher Cherry</b></TD>
+<TD><A HREF="mailto:cherry@rice.edu">cherry@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~brentc/"><b>Brent Thomas Christensen</b></A></TD>
+<TD><A HREF="mailto:brentc@rice.edu">brentc@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~jcloudm/"><b>John Andrew Francis Cloudman</b></A></TD>
+<TD><A HREF="mailto:jcloudm@rice.edu">jcloudm@rice.edu</A></TD></TR>
+<TR><TD><b>Marisa Barrera Cruz</b></TD>
+<TD><A HREF="mailto:mcruz@rice.edu">mcruz@rice.edu</A></TD></TR>
+<TR><TD><b>Eric Dai</b></TD>
+<TD><A HREF="mailto:edai@rice.edu">edai@rice.edu</A></TD></TR>
+<TR><TD><b>Noemie Devi Dharamraj</b></TD>
+<TD><A HREF="mailto:noemie@rice.edu">noemie@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~sedmison/"><b>Sean Damian Edmison</b></A></TD>
+<TD><A HREF="mailto:sedmison@rice.edu">sedmison@rice.edu</A></TD></TR>
+<TR><TD><b>Clayton Owens Finney</b></TD>
+<TD><A HREF="mailto:finney@warri.wireline.slb.com
+">finney@warri.wireline.slb.com
+</A></TD></TR>
+<TR><TD><b>Annette Christine Franco</b></TD>
+<TD><A HREF="mailto:afranco@rice.edu">afranco@rice.edu</A></TD></TR>
+<TR><TD><b>Efren Garcia</b></TD>
+<TD><A HREF="mailto:efren@rice.edu">efren@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~jase/"><b>Jason Arthur Gayman</b></A></TD>
+<TD><A HREF="mailto:Jason_A_Gayman@ccm.intel.com
+">Jason_A_Gayman@ccm.intel.com
+</A></TD></TR>
+<TR><TD><b>Ellis Robinson Giles</b></TD>
+<TD><A HREF="mailto:erg@rice.edu">erg@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~akgoel/"><b>Ankur Kumar Goel</b></A></TD>
+<TD><A HREF="mailto:akgoel@rice.edu">akgoel@rice.edu</A></TD></TR>
+<TR><TD><b>Heather Dawn Haiges</b></TD>
+<TD><A HREF="mailto:hhaiges@rice.edu">hhaiges@rice.edu</A></TD></TR>
+<TR><TD><b>Clayton Lee Hall</b></TD>
+<TD><A HREF="mailto:clhall@rice.edu">clhall@rice.edu</A></TD></TR>
+<TR><TD><b>William "Trey" Hamilton</b></TD>
+<TD><A HREF="mailto:trey@rice.edu">trey@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~jayh/"><b>James Henry Henderson</b></A></TD>
+<TD><A HREF="mailto:jayh@rice.edu">jayh@rice.edu</A></TD></TR>
+<TR><TD><b>Michael William Holman</b></TD>
+<TD><A HREF="mailto:mholman@rice.edu">mholman@rice.edu</A></TD></TR>
+<TR><TD><b>Yoon Mark Hong</b></TD>
+<TD><A HREF="mailto:ymhong@rice.edu">ymhong@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~grahamh/"><b>Graham S. Horn</b></A></TD>
+<TD><A HREF="mailto:grahamh@rice.edu">grahamh@rice.edu</A></TD></TR>
+<TR><TD><b>Naomi R Weiss Horowitz</b></TD>
+<TD><A HREF="mailto:naomih@rice.edu">naomih@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~huangkie/"><b>Michael Huang</b></A></TD>
+<TD><A HREF="mailto:huangkie@rice.edu">huangkie@rice.edu</A></TD></TR>
+<TR><TD><b>Kristopher Ghassan Karam</b></TD>
+<TD><A HREF="mailto:krisk@rice.edu">krisk@rice.edu</A></TD></TR>
+<TR><TD><b>Billy John Kim</b></TD>
+<TD><A HREF="mailto:billyk@rice.edu">billyk@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~davekim/"><b>David Tae Kim</b></A></TD>
+<TD><A HREF="mailto:davekim@rice.edu">davekim@rice.edu</A></TD></TR>
+<TR><TD><b>Christopher Row Klick</b></TD>
+<TD><A HREF="mailto:klick@rice.edu">klick@rice.edu</A></TD></TR>
+<TR><TD><b>Todd Randolph Konkel</b></TD>
+<TD><A HREF="mailto:zwieback@rice.edu">zwieback@rice.edu</A></TD></TR>
+<TR><TD><b>Marilyn Meng-lin Lau</b></TD>
+<TD><A HREF="mailto:maril@rice.edu">maril@rice.edu</A></TD></TR>
+<TR><TD><b>James Jin-Hong Ling</b></TD>
+<TD><A HREF="mailto:lingjj@rice.edu">lingjj@rice.edu</A></TD></TR>
+<TR><TD><b>Robert John Lonigro</b></TD>
+<TD><A HREF="mailto:roblon@rice.edu">roblon@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~slorimer/"><b>Stephen William Lorimer</b></A></TD>
+<TD><A HREF="mailto:slorimer@rice.edu">slorimer@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~platypus/"><b>David Alexander Lyons</b></A></TD>
+<TD><A HREF="mailto:platypus@rice.edu">platypus@rice.edu</A></TD></TR>
+<TR><TD><b>Deborah Lynn Lyons</b></TD>
+<TD><A HREF="mailto:debilyon@rice.edu">debilyon@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~matthewm/"><b>Matthew Charles Marlay</b></A></TD>
+<TD><A HREF="mailto:matthewm@rice.edu">matthewm@rice.edu</A></TD></TR>
+<TR><TD><b>Sergio Martinez</b></TD>
+<TD><A HREF="mailto:sergio@rice.edu">sergio@rice.edu</A></TD></TR>
+<TR><TD><b>David Keith Murgatroyd</b></TD>
+<TD><A HREF="mailto:dmurga@rice.edu">dmurga@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~von/"><b>Vuong Dinh Nguyen</b></A></TD>
+<TD><A HREF="mailto:von@rice.edu">von@rice.edu</A></TD></TR>
+<TR><TD><b>Ciro Tadashi Nishiguchi</b></TD>
+<TD><A HREF="mailto:cirotn@rice.edu">cirotn@rice.edu</A></TD></TR>
+<TR><TD><b>Jennifer Joy Rahmandar</b></TD>
+<TD><A HREF="mailto:jenrahm@rice.edu">jenrahm@rice.edu</A></TD></TR>
+<TR><TD><b>Vivek Chennagiri Rao</b></TD>
+<TD><A HREF="mailto:vivekr@rice.edu">vivekr@rice.edu</A></TD></TR>
+<TR><TD><b>Noah Aubrey Rosenberg</b></TD>
+<TD><A HREF="mailto:noahr@rice.edu">noahr@rice.edu</A></TD></TR>
+<TR><TD><b>Karen Diane Rossi</b></TD>
+<TD><A HREF="mailto:rossi@rice.edu">rossi@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~anders/"><b>Anders Christian Ryerson</b></A></TD>
+<TD><A HREF="mailto:anders@rice.edu">anders@rice.edu</A></TD></TR>
+<TR><TD><b>Kara Lee Schaefer</b></TD>
+<TD><A HREF="mailto:scarlet@rice.edu">scarlet@rice.edu</A></TD></TR>
+<TR><TD><b>Paul Adam Schepflin</b></TD>
+<TD><A HREF="mailto:blueyes@rice.edu">blueyes@rice.edu</A></TD></TR>
+<TR><TD><b>Alda Elizabeth Schwonke</b></TD>
+<TD><A HREF="mailto:alda@rice.edu">alda@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~shorey/"><b>Jamie Margaret Shorey</b></A></TD>
+<TD><A HREF="mailto:shorey@rice.edu">shorey@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~merry/"><b>Marla Annette Smith</b></A></TD>
+<TD><A HREF="mailto:merry@rice.edu">merry@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~seymore/"><b>Matthew Jeremy Tenny</b></A></TD>
+<TD><A HREF="mailto:seymore@rice.edu">seymore@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~gwent/"><b>Gwen Elizabeth Thomas</b></A></TD>
+<TD><A HREF="mailto:gwent@rice.edu">gwent@rice.edu</A></TD></TR>
+<TR><TD><b>Jonathan Jerome Tilghman</b></TD>
+<TD><A HREF="mailto:rico@rice.edu">rico@rice.edu</A></TD></TR>
+<TR><TD><b>Jennifer Marcella Tjia</b></TD>
+<TD><A HREF="mailto:tjia@rice.edu">tjia@rice.edu</A></TD></TR>
+<TR><TD><b>Aaron Dwight Trout</b></TD>
+<TD><A HREF="mailto:atrout@rice.edu">atrout@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~nickt/"><b>Nick Dimitrios Tsihlis</b></A></TD>
+<TD><A HREF="mailto:nickt@rice.edu">nickt@rice.edu</A></TD></TR>
+<TR><TD><b>Paulina Elaine Vinyard</b></TD>
+<TD><A HREF="mailto:pev@rice.edu">pev@rice.edu</A></TD></TR>
+<TR><TD><b>Matthew Laurence Weston</b></TD>
+<TD><A HREF="mailto:mweston@rice.edu">mweston@rice.edu</A></TD></TR>
+<TR><TD><b>Matthew Gregory Wylie</b></TD>
+<TD><A HREF="mailto:mwylie@rice.edu">mwylie@rice.edu</A></TD></TR>
+<TR><TD><b>Chi-Jye Jacob Yeh</b></TD>
+<TD><A HREF="mailto:jyeh@rice.edu">jyeh@rice.edu</A></TD></TR>
+<TR><TD><A target="_top" HREF="http://hc.hanszen.rice.edu/~zarate/"><b>Christopher Michael Zarate</b></A></TD>
+<TD><A HREF="mailto:zarate@rice.edu">zarate@rice.edu</A></TD></TR>
+</TABLE>
+</CENTER>
+<br>
+<br>
+
+</body>
+
+<table>
+  <tr>
+    <td colspan=2>
+      <nobr>
+        <img src=/Images/line-guardian-left.gif>
+      </nobr>
+    </td>
+  </tr>
+<!---
+  <tr>
+    <td align=center halign=top>
+      <img src=http://www.hanszenet.rice.edu/Images/mail.gif>
+    </td>
+    <td>
+      <nobr><b>Virtual Hanszen</b> is brought to you by the
+        <b><a href=http://www.hanszenet.rice.edu/HanszeNet/>Hanszen Computer Committee</a></b>
+      </nobr>
+      <br>
+      <nobr>Questions/comments about the site?  Direct them to
+        <a href=mailto:www@www.hanszenet.rice.edu>www@www.hanszenet.rice.edu</a>
+      </nobr>
+      <br>
+      <nobr>Problems with HanszeNet?  Direct them to
+        <a href=mailto:problem@hc.hansznet.rice.edu>problem@hc.hanszenet.rice.edu</a>
+      </nobr>
+    </td>
+  </tr>
+--->
+</table>
+
+</html>
